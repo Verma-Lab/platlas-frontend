305435a61640a51c1c52a7646fca497d5140bdf0
Hritvik Gupta <hritvik7654@gmail.com>
2025-03-11 10:42:23 -0400

    fixedtwicepull

--- src/pages/PhewasPage.js
+++ src/pages/PhewasPage.js
@@ -324,9 +324,13 @@ const PheWASHeader = ({ selectedSNP, phewasData, onMenuClick }) => {
               <div className="flex items-center">
                 <BarChart2 className="mr-4 h-10 w-10 text-white" />
                 <div>
-                  <h1 className="text-4xl font-bold text-white mb-2">
-                    PheWAS Analysis Page
-                  </h1>
+                  <div className="flex items-center">
+                    <h1 className="text-4xl font-bold text-white mb-2 mr-4">
+                      PheWAS Analysis Page
+                    </h1>
+                    {/* Added GeneralBar next to the page title */}
+                    <GenerlaBar />
+                  </div>
                   <div className="flex items-center">
                     <span className="text-blue-100 mr-4">SNP:</span>
                     <span className="bg-white/20 px-4 py-2 rounded-lg text-white font-semibold text-lg">
@@ -358,10 +362,7 @@ const PheWASHeader = ({ selectedSNP, phewasData, onMenuClick }) => {
           }}
         />
         
-        {/* Added GeneralBar component here */}
-        <div className='p-5 -mt-10'>
-          <GenerlaBar />
-        </div>
+        {/* Removed GeneralBar from here as it's now next to the title */}
       </div>
       
       <div className="max-w-7xl mx-auto">
